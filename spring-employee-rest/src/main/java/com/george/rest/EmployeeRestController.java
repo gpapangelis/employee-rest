5924c5940be71f7e9833086095ef7dfde929a982
george <george@georges-mbp>
2020-11-08 00:40:30 +0200

    Commit i dont know

--- spring-employee-rest/src/main/java/com/george/rest/EmployeeRestController.java
+++ spring-employee-rest/src/main/java/com/george/rest/EmployeeRestController.java
@@ -3,8 +3,12 @@ package com.george.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,5 +47,44 @@ public class EmployeeRestController {
 		
 		return theEmployee;
 	}
+	
+	//add mapping for post employee - add new employee
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+	
+		
+		
+		employeeService.saveEmployee(theEmployee);
+		
+		return theEmployee;
+	}
+	
+	//add mapping for put employees - update
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+		
+		
+		employeeService.saveEmployee(theEmployee);
+		return theEmployee;
+	}
+	
+	//add mapping for delete employee
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		
+		Employee tempEmployee = employeeService.getEmployee(employeeId);
+		
+		//exception if null
+		if (tempEmployee == null) {
+			
+			throw new EmployeeNotFoundException("Employee id not found- " + employeeId);
+			
+		}
+		
+		employeeService.deleteEmployee(employeeId);
+		
+		return "Deleted employee with id -" +employeeId;
+	}
+	
 	
 }
